f3ab0debd51195a64f0c9d1fdc9ca22bb1b7b09d
Miguel Yurivilca <miguel.yurivilca.16@gmail.com>
2020-04-01 13:40:33 -0500

    minor changes

--- hand_wash/src/main/java/cs_utec/cs2901/App.java
+++ hand_wash/src/main/java/cs_utec/cs2901/App.java
@@ -4,12 +4,10 @@ package cs_utec.cs2901;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-		for (int i = 0; i < 100; ++i) {
-			System.out.println("Me debo lavar las manos por 20 segundos");
-		}
+public class App {
+  public static void main(String[] args) {
+    for (int i = 0; i < 100; ++i) {
+      System.out.println("Me debo lavar las manos por 20 segundos");
     }
+  }
 }
