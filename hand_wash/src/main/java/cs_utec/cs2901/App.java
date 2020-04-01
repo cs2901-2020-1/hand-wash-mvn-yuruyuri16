4c93d4bc1f7296c948658783a69a80e844466a6c
Miguel Yurivilca <miguel.yurivilca.16@gmail.com>
2020-04-01 13:35:48 -0500

    Finished program

--- hand_wash/src/main/java/cs_utec/cs2901/App.java
+++ hand_wash/src/main/java/cs_utec/cs2901/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+		for (int i = 0; i < 100; ++i) {
+			System.out.println("Me debo lavar las manos por 20 segundos");
+		}
     }
 }
